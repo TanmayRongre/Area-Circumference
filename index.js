9f7273b0d39a52747160e22c0f702f1e1c55cbd2
Tanmay Rongre <tanmayrongre2008@gmail.com>
2026-01-08 22:36:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ let Radius;
 const PI = 3.14
 document.getElementById("MySubmit").onclick = function () {
     Radius = document.getElementById("MyText").value;
-    document.getElementById("p1").textContent = `Area of circle =  ${PI * PI * Radius} `
+    document.getElementById("p1").textContent = `Area of circle =  ${PI * Radius * Radius} `
     document.getElementById("p2").textContent = `Circumferene of circle =  ${2 * PI * Radius} `
 
 }
+
