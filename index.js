6414002253218d7f0f6c6a0161b30276d5ef372a
Tanmay Rongre <tanmayrongre2008@gmail.com>
2026-01-08 22:38:46 +0530

    Add units to area and circumference calculations

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ let Radius;
 const PI = 3.14
 document.getElementById("MySubmit").onclick = function () {
     Radius = document.getElementById("MyText").value;
-    document.getElementById("p1").textContent = `Area of circle =  ${PI * Radius * Radius} `
-    document.getElementById("p2").textContent = `Circumferene of circle =  ${2 * PI * Radius} `
+    document.getElementById("p1").textContent = `Area of circle =  ${PI * Radius * Radius} cm^2 `
+    document.getElementById("p2").textContent = `Circumferene of circle =  ${2 * PI * Radius} cm `
 
 }
+
 
